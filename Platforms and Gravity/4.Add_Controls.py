c084a8e4d03ddbf1992df174a318f971229be25b
carolyn.brodie <carolyn.brodie@simpson.edu>
2025-11-08 11:26:27 -0600

    Gravity tutorial files based on website

--- Platforms and Gravity/4.Add_Controls.py
+++ Platforms and Gravity/4.Add_Controls.py
@@ -1,126 +1,236 @@
 """
+
 Platformer Game
+
+
+python -m arcade.examples.platform_tutorial.04_user_control
+
 """
+
 import arcade
 
+
 # Constants
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 650
-SCREEN_TITLE = "Platformer"
+
+WINDOW_WIDTH = 1280
+
+WINDOW_HEIGHT = 720
+
+WINDOW_TITLE = "Platformer"
+
 
 # Constants used to scale our sprites from their original size
-CHARACTER_SCALING = 1
+
 TILE_SCALING = 0.5
 
+
 # Movement speed of player, in pixels per frame
+
 PLAYER_MOVEMENT_SPEED = 5
 
 
-class MyGame(arcade.Window):
+
+class GameView(arcade.Window):
+
     """
+
     Main application class.
+
     """
+
 
     def __init__(self):
 
+
         # Call the parent class and set up the window
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
-        # Our Scene Object
-        self.scene = None
+        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
 
-        # Separate variable that holds the player sprite
-        self.player_sprite = None
 
-        # Our physics engine
-        self.physics_engine = None
+        # Variable to hold our texture for our player
 
-        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+        self.player_texture = arcade.load_texture(
 
-    def setup(self):
-        """Set up the game here. Call this function to restart the game."""
+            ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
 
-        # Initialize Scene
-        self.scene = arcade.Scene()
-
-        # Set up the player, specifically placing it at these coordinates.
-        image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
-        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
-        self.player_sprite.center_x = 64
-        self.player_sprite.center_y = 128
-        self.scene.add_sprite("Player", self.player_sprite)
-
-        # Create the ground
-        # This shows using a loop to place multiple sprites horizontally
-        for x in range(0, 1250, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
-            wall.center_x = x
-            wall.center_y = 32
-            self.scene.add_sprite("Walls", wall)
-
-        # Put some crates on the ground
-        # This shows using a coordinate list to place sprites
-        coordinate_list = [[512, 96], [256, 96], [768, 96]]
-
-        for coordinate in coordinate_list:
-            # Add a crate on the ground
-            wall = arcade.Sprite(
-                ":resources:images/tiles/boxCrate_double.png", TILE_SCALING
-            )
-            wall.position = coordinate
-            self.scene.add_sprite("Walls", wall)
-
-        # Create the 'physics engine'
-        self.physics_engine = arcade.PhysicsEngineSimple(
-            self.player_sprite, self.scene.get_sprite_list("Walls")
         )
 
+
+        # Separate variable that holds the player sprite
+
+        self.player_sprite = arcade.Sprite(self.player_texture)
+
+        self.player_sprite.center_x = 64
+
+        self.player_sprite.center_y = 128
+
+
+        # SpriteList for our player
+
+        self.player_list = arcade.SpriteList()
+
+        self.player_list.append(self.player_sprite)
+
+
+        # SpriteList for our boxes and ground
+
+        # Putting our ground and box Sprites in the same SpriteList
+
+        # will make it easier to perform collision detection against
+
+        # them later on. Setting the spatial hash to True will make
+
+        # collision detection much faster if the objects in this
+
+        # SpriteList do not move.
+
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+
+
+        # Create the ground
+
+        # This shows using a loop to place multiple sprites horizontally
+
+        for x in range(0, 1250, 64):
+
+            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", scale=TILE_SCALING)
+
+            wall.center_x = x
+
+            wall.center_y = 32
+
+            self.wall_list.append(wall)
+
+
+        # Put some crates on the ground
+
+        # This shows using a coordinate list to place sprites
+
+        coordinate_list = [[512, 96], [256, 96], [768, 96]]
+
+
+        for coordinate in coordinate_list:
+
+            # Add a crate on the ground
+
+            wall = arcade.Sprite(
+
+                ":resources:images/tiles/boxCrate_double.png", scale=TILE_SCALING
+
+            )
+
+            wall.position = coordinate
+
+            self.wall_list.append(wall)
+
+
+        # Create a Simple Physics Engine, this will handle moving our
+
+        # player as well as collisions between the player sprite and
+
+        # whatever SpriteList we specify for the walls.
+
+        self.physics_engine = arcade.PhysicsEngineSimple(
+
+            self.player_sprite, self.wall_list
+
+        )
+
+
+        self.background_color = arcade.csscolor.CORNFLOWER_BLUE
+
+
+    def setup(self):
+
+        """Set up the game here. Call this function to restart the game."""
+
+        pass
+
+
     def on_draw(self):
+
         """Render the screen."""
 
+
         # Clear the screen to the background color
+
         self.clear()
 
-        # Draw our Scene
-        self.scene.draw()
 
-    def on_key_press(self, key, modifiers):
-        """Called whenever a key is pressed."""
+        # Draw our sprites
 
-        if key == arcade.key.UP or key == arcade.key.W:
-            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
-        elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
-        elif key == arcade.key.LEFT or key == arcade.key.A:
-            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
-        elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+        self.player_list.draw()
 
-    def on_key_release(self, key, modifiers):
-        """Called when the user releases a key."""
+        self.wall_list.draw()
 
-        if key == arcade.key.UP or key == arcade.key.W:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.LEFT or key == arcade.key.A:
-            self.player_sprite.change_x = 0
-        elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.player_sprite.change_x = 0
 
     def on_update(self, delta_time):
-        """Movement and game logic"""
 
-        # Move the player with the physics engine
+        """Movement and Game Logic"""
+
+
+        # Move the player using our physics engine
+
         self.physics_engine.update()
 
 
+    def on_key_press(self, key, modifiers):
+
+        """Called whenever a key is pressed."""
+
+
+        if key == arcade.key.UP or key == arcade.key.W:
+
+            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+
+            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+
+    def on_key_release(self, key, modifiers):
+
+        """Called whenever a key is released."""
+
+
+        if key == arcade.key.UP or key == arcade.key.W:
+
+            self.player_sprite.change_y = 0
+
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+
+            self.player_sprite.change_y = 0
+
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+
+            self.player_sprite.change_x = 0
+
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+
+            self.player_sprite.change_x = 0
+
+
+
 def main():
+
     """Main function"""
-    window = MyGame()
+
+    window = GameView()
+
     window.setup()
+
     arcade.run()
 
 
+
 if __name__ == "__main__":
+
     main()
